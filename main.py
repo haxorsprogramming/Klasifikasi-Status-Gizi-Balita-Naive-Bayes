af77f951b54c907edf085da52dfae107bef80c11
Haxorsprogramming <haxors@uinsu.ac.id>
2022-07-24 18:43:39 +0700

    'update'

--- main.py
+++ main.py
@@ -12,10 +12,35 @@ def index():
 
 @app.route('/data-balita')
 def data_balita():
-    dataBalita = pd.read_excel("data/DATA_DUMMY.xlsx", index_col=0)
+    dBalita = []
+    dataBalita = pd.read_excel("data/DATA_DUMMY.xlsx")
     dbnp = dataBalita.to_numpy()
-    
-    return render_template('data-balita.html')
+    ord = 0
+    for x in dbnp:
+        # print(x[6])
+        dSatuan = {}
+        dSatuan['ord'] = ord
+        dSatuan['nama'] = x[0]
+        dSatuan['jk'] = x[1]
+        dSatuan['usia'] = x[2]
+        dSatuan['berat_badan'] = x[3]
+        dSatuan['tinggi_badan'] = x[4]
+        dSatuan['lila'] = x[5]
+        dSatuan['kelas_gizi'] = x[6]
+        dBalita.append(dSatuan)
+        ord += 1
+        print(x[1])
+    # print(dBalita)
+    return render_template('data-balita.html', dBalita=dBalita)
+
+@app.route('/data-latih')
+def data_latih():
+    dBalita = []
+    dataBalita = pd.read_excel("data/DATA_DUMMY.xlsx")
+    dataBalita.replace({'Jenis_Kelamin':{'JK1':1, 'JK2':2}},inplace=True)
+    dataBalita.replace({'Usia':{'U1':1}},inplace=True)
+    print(dataBalita)
+    return render_template('data-latih.html')
 
 # if __name__ == '__main__':
 app.run(host='0.0.0.0', port=7001)
